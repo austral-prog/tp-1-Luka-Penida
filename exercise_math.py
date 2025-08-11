941d099d9eba4d398462efa884f17566c4522f00
Luka-Penida <lpenida1@mail.austral.edu.ar>
2025-08-11 12:35:40 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,21 @@
 def math():
     a = 57
     b = 7
+    suma = (a + b)
+    print(suma) #suma
+    resta = a - b
+    print(resta) #resta
+    multiplicación = a * b
+    print(multiplicación) #producto
+    división = (a + b) / 2
+    print(división) #promedio
+    cociente_entero = a // b
+    print(cociente_entero) #cociente entero
+    resto_división = a % b
+    print(resto_división) #resto
+    valor_real = a / b
+    print(valor_real) #valor real
+
+
+
+math()
